6a8463e921f95ad2f618cf00d50c8041500b2dc8
JMSandoval87 <LordJonas@gmail.com>
2021-09-20 14:06:16 -0700

    ~

--- lib/set_alarm.dart
+++ lib/set_alarm.dart
@@ -1,35 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/set_alarm.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class SetAlarmPage extends StatefulWidget {
+  SetAlarmPage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -43,10 +15,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SetAlarmPageState createState() => _SetAlarmPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _SetAlarmPageState extends State<SetAlarmPage> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -73,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       //can remove appBar if you need to
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the SetAlarmPage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
@@ -98,10 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Some sample text - SCREEN 1',
+              'Some sample text - SCREEN 2',
             ),
             Text(
-              'more sample text - SCREEN 1',
+              'more sample text- SCREEN 2',
             ),
             Text(
               'You have pushed the button this many times:',
